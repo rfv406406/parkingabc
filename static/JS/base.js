ce27b0cde9beea549a209e980687961afdc5eab4
Zhui Kang <rfv406406@gmail.com>
2023-11-08 20:37:50 +0800

    登入HTML

---
+++ static/JS/base.js
@@ -0,0 +1,6 @@
+document.querySelector('#menu').addEventListener('click', function() {
+    let menuContent = document.querySelector('#menuContent');
+    menuContent.classList.toggle('toggle-menu');
+  });
+
+
