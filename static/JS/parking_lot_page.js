978a4a13bbf6f3a6d0b001e33e2b86fe14aa8190
Zhui Kang <rfv406406@gmail.com>
2023-11-13 19:36:31 +0800

    add20231113

--- static/JS/parking_lot_page.js
+++ static/JS/parking_lot_page.js
@@ -7,9 +7,14 @@ addButton.addEventListener('click', addInputField);
 function addInputField() {
     inputCount++;
     const inputBoxContainer = document.getElementById('input-box-container');
-    const newInputBox = document.createElement('div');
-    newInputBox.className = 'input-box';
-    newInputBox.innerHTML = `<label for="parking-lot-number${inputCount}" class="consistent-text">車位編號${inputCount}：</label>
-                             <input type="text" id="parking-lot-number${inputCount}" name="parking-lot-number" class="text" placeholder="請輸入車牌編號">`;
-    inputBoxContainer.appendChild(newInputBox);
+    const newInputBoxNumber = document.createElement('div');
+    const newInputBoxImage = document.createElement('div');
+    newInputBoxNumber.className = 'input-box';
+    newInputBoxImage.className = 'input-box';
+    newInputBoxNumber.innerHTML = `<label for="parking-lot-number${inputCount}" class="consistent-text">車位編號${inputCount}：</label>
+    <input type="text" id="parking-lot-number${inputCount}" name="parking-lot-number" class="text" placeholder="請輸入車牌編號">`;
+    newInputBoxImage.innerHTML = `<label for="parking-square-image${inputCount}" class="consistent-text">車位圖片${inputCount}：</label>
+    <input type="file" id="parking-square-image${inputCount}" name="parking-square-image-information" class="text" placeholder="車位圖片">`;
+    inputBoxContainer.appendChild(newInputBoxNumber);
+    inputBoxContainer.appendChild(newInputBoxImage);
 }  
